b4ae2166052f4ebd637e4a434e8a0d0721014be7
Shweta Gidde <shwetagidde7@gmail.com>
2026-01-23 12:02:01 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		int value1 = 10 , value2;
+		value2 = value1*20;
+		
+		System.out.println(value2);
+		
+		boolean var;
+		var = true;
+		System.out.println(var);
+		
+		char var1;
+		var1 = 's;';
+		System.out.println(var1);
+
+	}
+
+}
