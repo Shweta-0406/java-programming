8216198ab68a5b9bcc73e0828d303e4adb11e62f
Shweta Gidde <shwetagidde7@gmail.com>
2026-01-27 13:10:02 +0530

    Add files via upload

---
+++ Forloopeg1.java
@@ -0,0 +1,16 @@
+
+public class Forloopeg1 {
+
+	public static void main(String[] args) {
+		
+		
+		for( int i = 1; i<=10; ++i)
+		{
+			System.out.print(i + "  ");
+		}
+		//System.out.println(i);
+		
+
+	}
+
+}
