63582165c0120f714701a52ff724eec0f6b7deb3
Shweta Gidde <shwetagidde7@gmail.com>
2026-01-27 13:01:28 +0530

    Add files via upload

---
+++ StringBasics.java
@@ -0,0 +1,20 @@
+
+public class StringBasics {
+
+	public static void main(String[] args) {
+		
+		String var1 = "Study";
+		String var2 = "Easy";
+		int var3 = 10;
+		
+		System.out.println(var1+var2+var3);
+		
+		String var4 ="100";
+		String var5 ="20";
+		System.out.println(Integer.parseInt(var4)+var3);
+		System.out.println(var4+var5);
+		//System.out.println(var4);
+
+	}
+
+}
