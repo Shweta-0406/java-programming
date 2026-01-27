6a4633efed9059228243cdad6054f8adda2d8ced
Shweta Gidde <shwetagidde7@gmail.com>
2026-01-27 13:12:52 +0530

    Add files via upload

---
+++ While.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class While {
+
+	public static void main(String[] args) {
+		
+		/*int n;
+		System.out.println("Enter value for condition :");
+		Scanner obj=new Scanner(System.in);
+		n=obj.nextInt();
+		
+		while(n>=6)
+		{
+			System.out.println("Number is positive...");
+		}*/
+        int n=1;
+        while(n<=10)
+        {
+        	System.out.println("Learning Coding...");
+        	n++;
+        }
+	}
+
+}
